704128208767aa3db397cce2000470da4cf82b95
Minalinsky02 <74691266+Minalinsky02@users.noreply.github.com>
2025-08-18 17:19:14 +0800

    Add text colour to css, js tp html, re did some parts of the js

--- background-mover.js
+++ background-mover.js
@@ -1,44 +1,53 @@
 const track = document.getElementById("image-track");
 
-// tracking starting point of per mouse click
-window.onmousedown = e =>
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () =>
 {
-    e.clientX;
-    track.dataset.mouseDownAt = e.clientX;
+    track.dataset.mouseDownAt = "0";
     track.dataset.prevPercentage = track.dataset.percentage;
 }
 
-window.onmouseup = () =>
-{
-    track.dataset.mouseDownAt = "0";
-}
-
-// mouse slider as it spans across the scene
-window.onmousemove = e =>
+const handleOnMove = e => 
 {
     if(track.dataset.mouseDownAt === "0") return;
-    const mouseDelta = parseFloat(track.dataset.nmouseDownAt) - e.clientX,
-            maxDelta = window.innerWidth /2;
 
-    const percentage = (mouseDelta / maxDelta) * -100;
-        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-        Math.min(nextPercentage, 0);
-        Math.max(nextPercentage, -100);
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+
+    const percentage = (mouseDelta /maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
     track.dataset.percentage = nextPercentage;
 
-    track.style.transform = 'translate(${percentage}%, -50%)';
-
     track.animate(
-        {
-            transform: 'translate(${nextPercentage}%, -50%)'
-        },
-        {
-            duration: 100, fill:"forwards"
-        }
-    );
-
+    {
+        transform: `translate(${nextPercentage}%, -50%)`
+    }, 
+    { 
+        duration: 1200, fill: "forwards"
+    });
+  
+  for(const image of track.getElementsByClassName("image")) 
+    {
     image.animate({
-        objectPosition: '${100 + nextPercentage}% center'
-    }, {duration: 1200, fill: "forwards"});
+      objectPosition: `${100 + nextPercentage}% center`
+    }, 
+    { duration: 1200, fill: "forwards" });
+    }
 }
+
+
+// tracking starting point of per mouse click
+window.onmousedown = e => handleOnDown(e);
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+
